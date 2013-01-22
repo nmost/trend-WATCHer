f1820e8eb287fe08544d7007013e1e137b0709d4
nmost <nick@mostowich.com>
2013-01-22 18:57:39 -0500

    add initial gcm pingage

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+//mongoose includes
 var mongoose = require("mongoose")
 var uri =   process.env.MONGODB_URI || 
   process.env.MONGOLAB_URI || 
@@ -11,18 +12,73 @@ db = mongoose.connect(uri,  function (err, res) {
 });
 Schema = mongoose.Schema;
 
+//Express stuff
 var express = require('express');
 var server = express();
 server.use(express.bodyParser());
 
+//GCM stuff
+var gcm = require('node-gcm');
 
-var PersonSchema = new Schema({
-  first_name: String,
-  last_name: String,
-  is_awesome: Boolean
+//DATABASE INIT
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var UserSchema = new Schema({
+  registration_id: String,
+  trend: { type: ObjectId, ref: 'Trend'},
+  is_watching: { type: Boolean, default: false}
 });
-Person = mongoose.model('Person', PersonSchema);
+var TrendSchema = new Schema({
+  trend: String,
+  tweets: { type: ObjectId, ref: 'Tweet'},
+  users: {type: ObjectId, ref: 'User'},
+  newcounter: Number
+});
+var TweetSchema = new Schema({
+  tweeter: String,
+  time: { type: Date, default: Date.now },
+  content: String,
+  trend: { type: ObjectId, ref: 'Trend'},
+});
 
+User = mongoose.model('User', UserSchema);
+Trend = mongoose.model('Trend', TrendSchema);
+Tweet = mongoose.model('Tweet', TweetSchema);
+
+
+//////
+//Initialize a user
+////
+function initializeUser(req, res, next){
+  var user = new User();
+  user.registration_id = req.body.registration_id;
+  user.save(function(){
+    res.send(user);
+    pingUser(user.registration_id);
+  });
+}
+
+
+//////
+//Set the currently trending topic
+////
+function setTrending(req, res, next){
+
+
+}
+
+
+//////
+//Post the tweets to a user
+////
+function pingUser(ids){
+  var message = new gcm.Message();
+  var sender = new gcm.Sender('AIzaSyDx1b8eGfFYEmAgrwp7qgTwU3SSU9_1mu4');
+  
+  sender.send(message, ids, 5, function(err, result){
+    console.log(result);
+  });
+}
+/*
 function getPeople(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -46,7 +102,8 @@ function postPerson(req,res,next) {
 }
 
 server.get('/people', getPeople);
-server.post('/people', postPerson);
+server.post('/people', postPerson);*/
+server.post('/newuser', initializeUser);
 var port = process.env.PORT || 8080;
 server.listen(port, function(){
   console.log('listening at %s', port);
